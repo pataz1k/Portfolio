7d9e660638eb9d86d74433eb02d42deece0e4e1f
pataz1k <alekseiloker1@gmail.com>
2025-01-07 17:41:47 +0500

    fix project list error

--- src/pages/Projects/ProjectsList.tsx
+++ src/pages/Projects/ProjectsList.tsx
@@ -12,8 +12,8 @@ const ProjectsList: FC<IProjectsList> = ({ projects }) => {
 		<div className="container mx-auto px-4 py-8">
 			<h2 className="mb-8 text-3xl font-bold text-white">My Projects</h2>
 			<div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2">
-				{projects.map((project) => (
-					<ProjectItem key={project.title} project={project} />
+				{projects.map((project, index) => (
+					<ProjectItem key={project.title} index={index} project={project} />
 				))}
 			</div>
 		</div>
